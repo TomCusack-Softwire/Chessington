109ece8a08b8e7f63a8ac2ced14470fc4c6ae860
Tom Cusack <Tom.Cusack@softwire.com>
2022-04-27 10:55:15 +0100

    Red-5 Queen moves

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +7,29 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let position = board.findPiece(this);
+        let available = [];
+        // Rook
+        for (let counter = 0; counter < 8; counter++) {
+            if (counter !== position.row) {
+                available.push(Square.at(counter, position.col));
+            }
+            if (counter !== position.col) {
+                available.push(Square.at(position.row, counter));
+            }
+        }
+        // Bishop
+        for (let rowDir of [1, -1]) {
+            for (let colDir of [1, -1]) {
+                let row = position.row + rowDir;
+                let col = position.col + colDir;
+                while (0 <= row && row < 8 && 0 <= col && col < 8) {
+                    available.push(Square.at(row, col));
+                    row += rowDir;
+                    col += colDir;
+                }
+            }
+        }
+        return available;
     }
 }

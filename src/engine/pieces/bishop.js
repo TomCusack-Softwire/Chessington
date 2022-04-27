fbb369abbdda15e44f8110c5b24ed3a188ba609e
Tom Cusack <Tom.Cusack@softwire.com>
2022-04-27 10:50:25 +0100

    Red-4 Bishop moves

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,21 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let position = board.findPiece(this);
+        let available = [];
+
+        for (let rowDir of [1, -1]) {
+            for (let colDir of [1, -1]) {
+                let row = position.row + rowDir;
+                let col = position.col + colDir;
+                while (0 <= row && row < 8 && 0 <= col && col < 8) {
+                    available.push(Square.at(row, col));
+                    row += rowDir;
+                    col += colDir;
+                }
+            }
+        }
+
+        return available;
     }
 }

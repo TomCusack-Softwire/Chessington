9f9c698a6f0305ed659dae701252487f2d578e6f
Tom Cusack <Tom.Cusack@softwire.com>
2022-04-27 12:06:44 +0100

    Red-11 Queen can't move if blocked

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -7,29 +7,6 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        let position = board.findPiece(this);
-        let available = [];
-        // Rook
-        for (let counter = 0; counter < 8; counter++) {
-            if (counter !== position.row) {
-                available.push(Square.at(counter, position.col));
-            }
-            if (counter !== position.col) {
-                available.push(Square.at(position.row, counter));
-            }
-        }
-        // Bishop
-        for (let rowDir of [1, -1]) {
-            for (let colDir of [1, -1]) {
-                let row = position.row + rowDir;
-                let col = position.col + colDir;
-                while (Square.isValid(row, col)) {
-                    available.push(Square.at(row, col));
-                    row += rowDir;
-                    col += colDir;
-                }
-            }
-        }
-        return available;
+        return this.loopOverRowsAndCols(board, [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]);
     }
 }

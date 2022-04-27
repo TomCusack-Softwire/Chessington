69275cb9ecc7deeeee668d6f1bc1b679e75043bb
Tom Cusack <Tom.Cusack@softwire.com>
2022-04-27 11:45:18 +0100

    Red-9 Rooks doesn't move if blocked

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -9,14 +9,23 @@ export default class Rook extends Piece {
     getAvailableMoves(board) {
         let position = board.findPiece(this);
         let available = [];
-        for (let counter = 0; counter < 8; counter++) {
-            if (counter !== position.row) {
-                available.push(Square.at(counter, position.col));
+
+        for (let [rowDir, colDir] of [[1, 0], [-1, 0], [0, 1], [0, -1]]) {
+            let row = position.row + rowDir;
+            let col = position.col + colDir;
+            let square = Square.at(row, col);
+            while (Square.isValid(row, col) && board.getPiece(square) === undefined) {
+                available.push(square);
+                row += rowDir;
+                col += colDir;
+                square = Square.at(row, col);
             }
-            if (counter !== position.col) {
-                available.push(Square.at(position.row, counter));
+            // if other player's piece is there, can also take
+            if (Square.isValid(row, col) && board.getPiece(square).player !== this.player) {
+                available.push(square);
             }
         }
+
         return available;
     }
 }

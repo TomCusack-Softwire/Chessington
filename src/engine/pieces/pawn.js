5f52d53f0d2e27f1f960939b16fb83bb724749c1
Tom Cusack <Tom.Cusack@softwire.com>
2022-04-27 11:33:41 +0100

    Red-8 Pawn doesn't move if blocked

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -10,10 +10,20 @@ export default class Pawn extends Piece {
     getAvailableMoves(board) {
         let position = board.findPiece(this);
         let sign = (this.player === Player.WHITE ? 1 : -1);
-        let available = [Square.at(position.row + sign, position.col)];
-        if ((this.player === Player.WHITE && position.row === 1) || (this.player === Player.BLACK && position.row === 6)) {
-            available.push(Square.at(position.row + 2 * sign, position.col));
+        let available = [];
+
+        function checkAndAdd(row, col) {
+            let square = Square.at(row, col);
+            if (Square.isValid(row, col) && board.getPiece(square) === undefined) {
+                available.push(square);
+            }
         }
+
+        checkAndAdd(position.row + sign, position.col);
+        if (((this.player === Player.WHITE && position.row === 1) || (this.player === Player.BLACK && position.row === 6)) && board.getPiece(Square.at(position.row + sign, position.col)) === undefined) {
+            checkAndAdd(position.row + 2 * sign, position.col);
+        }
+
         return available;
     }
 }
